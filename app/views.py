64de33339bc63544904ad8ccae3b267eea143ccf
Felix Bade <flix.bade@gmail.com>
2021-05-31 00:49:24 +0300

    Add table

--- app/views.py
+++ app/views.py
@@ -17,8 +17,31 @@ def index():
 
     return render_template('frontpage.html', metrics=metrics)
 
+def get_week_data(metric, week):
+    year, weeknumber = week
+    return {
+        'week': {
+            'year': year,
+            'weeknumber': weeknumber
+        },
+        'metric': metric,
+        'days': [
+            {
+                'weekday': weekday,
+                'log_event': None
+            }
+            for weekday in range(7)
+        ]
+    }
+
 @app.route('/metric/<int:metric_id>')
 def view_metric(metric_id):
     metric = Metric.query.filter_by(id=metric_id).first()
+    year, weeknumber, weekday = datetime.now().isocalendar()
+    weeks = []
+    weeks.append(get_week_data(metric, (year, weeknumber-1)))
+    weeks.append(get_week_data(metric, (year, weeknumber)))
 
-    return str(metric.name)
+
+    return render_template('metric.html', metric_name=metric.name, weeks=weeks)
+
